68d0c112215328a85c1df00e31c050f9e588eb45
Intae Kim <greatkit@gmail.com>
2014-02-17 15:30:07 +0900

    some member work.

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,12 +1,11 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
-  test "email should be valid form" do
-    member = Member.new name:'Test', email:'test@example.org'
-    assert_equal 'Test', member.name
+  test "should have name and email" do
+    member = Member.new
+    member.name ='name'
+    member.email ='abc@example.com'
+    assert member.save
   end
 end
