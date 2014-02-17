5d152dcf3ab30be780aaa2d2f2e748d92775b6b9
Intae Kim <greatkit@gmail.com>
2014-02-17 10:12:38 +0900

    route for signup.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ AddressBook::Application.routes.draw do
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
 #  match '/about',   to: 'static_pages#about',   via: 'get'
+  match 'signup',   to: 'members#new',          via: 'get'
 
   get "members/new"
 
--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -4,6 +4,9 @@ class MembersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+
+    assert_template :new
+    assert_select 'title', text:'Address Book (Prototype) App | Sign up'
   end
 
 end
