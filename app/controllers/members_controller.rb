102e46d73edac04f23f95183fe44f6bfaca3d025
Intae Kim <greatkit@gmail.com>
2014-02-18 11:40:42 +0900

    7.3.2 strong parameters implementation.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,20 @@ class MembersController < ApplicationController
   end
 
   def new
-    @member = Member.new
   end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def member_params
+      params.require(:member).permit(:name, :email, :password, :password_confirmation)
+    end
 end
